c32343086ba1db2b25d6187d60a193709fdd230d
Emma <emmaborghult@gmail.com>
2019-05-02 00:54:16 +0200

    Adjusted js + changed design

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
-const colors = [
-  'rgb(66, 134, 244)',
-  'rgb(206, 66, 244)',
-  'rgb(244, 65, 107)',
-  'rgb(244, 232, 65)',
-  'rgb(124, 244, 65)',
-  'rgb(73, 251, 255)',
-];
-
-const squares = document.querySelectorAll('.square');
+const circles = document.querySelectorAll('.circles');
 const colorDisplay = document.querySelector('#color-display');
 const msgDisplay = document.querySelector('#result');
-const randomColor = (() => {
+const h1 = document.querySelector('h1');
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const generateRandomColors = ((num) => {
+  const arr = [];
+  for (let j = 0; j < num; j += 1) {
+    arr.push(randomColor());
+  }
+  return arr;
+});
+
+const colors = generateRandomColors(6);
+
+function pickColor() {
   const random = Math.floor(Math.random() * colors.length);
   return colors[random];
-});
-const secretColor = randomColor();
+}
 
-colorDisplay.textContent = secretColor;
+const pickedColor = pickColor();
 
-for (let i = 0; i < squares.length; i += 1) {
+const changeColor = (color) => {
+  for (let w = 0; w < circles.length; w += 1) {
+    circles[w].style.backgroundColor = color;
+  }
+};
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < circles.length; i += 1) {
   // inital colors for the squares
-  squares[i].style.backgroundColor = colors[i];
+  circles[i].style.backgroundColor = colors[i];
 
   // click listeners
-  squares[i].addEventListener('click', () => {
+  circles[i].addEventListener('click', () => {
     // get color of picked square
-    const clickedColor = squares[i].style.backgroundColor;
+    const clickedColor = circles[i].style.backgroundColor;
 
     // compare color to pickedColor
-    if (clickedColor === secretColor) {
+    if (clickedColor === pickedColor) {
       msgDisplay.textContent = 'YAY! YOU GOT IT RIGHT!';
-      const changeColor = (color) => {
-        for (let w = 0; w < squares.length; w += 1) {
-          squares[w].style.backgroundColor = color;
-        }
-      };
       changeColor(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       msgDisplay.textContent = 'Nope. Try again!';
-      squares[i].style.backgroundColor = '#333';
+      circles[i].style.backgroundColor = 'black';
     }
   });
 }
